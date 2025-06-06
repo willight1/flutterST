6607a1fbdb7cb5df1edee01546e4af802e3c271d
junho <willight1@naver.com>
2025-06-06 13:07:12 +0900

    Connected ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -10,10 +12,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: "AIzaSyDWFOMVkv2dhLaEiXhL62M80DDYpR59rKM",
-      appId: "1:1012631583207:android:f85d4430098f780a29a542",
+      apiKey: Platform.isAndroid ? "AIzaSyDWFOMVkv2dhLaEiXhL62M80DDYpR59rKM" : 'AIzaSyCfu29frdbeyOE7XWlRzg-Bf7Ns8XgKQGI',
+      appId: Platform.isAndroid ? "1:1012631583207:android:f85d4430098f780a29a542" : '1:1012631583207:ios:161bb85c5837d3cb29a542',
       messagingSenderId: "1012631583207",
       projectId: "thread-sample-app-43087",
+      iosBundleId: "com.example.threadAppSample",
     ),
   );
   runApp(const MyApp());
