df5eb5ebbce7ee4317b33a2826d1c76e0a713c6d
junho <willight1@naver.com>
2025-06-06 12:29:42 +0900

    firebase Android connect

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // ✅ 수정된 부분
 }
 
 android {
     namespace = "com.example.thread_app_sample"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,10 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.thread_app_sample"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +30,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +37,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:thread_app_sample/home.dart';
 import 'package:thread_app_sample/home_feed_list_controller.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDWFOMVkv2dhLaEiXhL62M80DDYpR59rKM",
+      appId: "1:1012631583207:android:f85d4430098f780a29a542",
+      messagingSenderId: "1012631583207",
+      projectId: "thread-sample-app-43087",
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -13,6 +24,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseFirestore.instance.collection('feeds').get().then((value) {
+      print(value.docs.length);
+    });
     timeago.setLocaleMessages('ko', timeago.KoMessages());
     Get.put(HomeFeedListcontroller());
     return GetMaterialApp(
